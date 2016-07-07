5561aa050592bac7e556afbb31b1f039711c31ac
Alejandro Cortes <Alejandro Cortes>
2016-07-06 21:10:12 -0400

    missing paras

--- server.js
+++ server.js
@@ -7,18 +7,16 @@ app.get('/', function(req, res) {
 	res.send('Hello W0rld');
 });
 
-
 // static route
 app.get('/meow', function(req, res) {
 	res.send('cat');
 });
 
-
 // dynamic route
 app.get('/meow/:something', function(req, res) {
 	console.log(req.params);
 	res.send(req.params.something);
-}
+});
 
 app.listen(PORT, function() {
 	console.log("I think I hear something...");
